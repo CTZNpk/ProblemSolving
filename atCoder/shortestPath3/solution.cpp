6f437c804a9401ee97de59ee593f785f70d77fb9
CTZNpk <haidersultan7675@gmail.com>
2024-10-01 21:18:46 +0500

    Solved using djikstra

---
+++ atCoder/shortestPath3/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void djikstra(int start, vector<ll> &dist,
+              vector<vector<pair<int, int>>> &adj) {
+  priority_queue<pair<ll, int>> pq;
+  pq.push({0, start});
+  dist[start] = 0;
+  while (pq.size()) {
+    pair<ll, int> curr = pq.top();
+    pq.pop();
+    ll d = -curr.first;
+    int u = curr.second;
+    if (dist[u] < d)
+      continue;
+    for (pair<int, int> edge : adj[u]) {
+      int v = edge.first, w = edge.second;
+      if (dist[v] > d + w) {
+        dist[v] = d + w;
+        pq.push({-d - w, v});
+      }
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n);
+  for (auto &i : a) {
+    cin >> i;
+  }
+  vector<vector<pair<int, int>>> adj(n);
+  for (int i = 0; i < m; i++) {
+    int u, v, b;
+    cin >> u >> v >> b;
+    adj[u - 1].push_back({v - 1, b + a[v - 1]});
+    adj[v - 1].push_back({u - 1, b + a[u - 1]});
+  }
+  vector<ll> dist(n, 1ll << 60);
+  djikstra(0, dist, adj);
+  for (int i = 1; i < n; i++) {
+    cout << dist[i] + a[0] << " ";
+  }
+
+  return 0;
+}
